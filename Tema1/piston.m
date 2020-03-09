17efc9d2b955a020854e73a6f41cd8083258d520
i62lucum <i62lucum@uco.es>
2020-03-09 10:44:07 +0100

    Pistas

--- Tema1/piston.m
+++ Tema1/piston.m
@@ -20,8 +20,12 @@ for i=2:length(t)
    phi=0+w*t(i);
    x1=NewtonRaphson(@funcionPiston,@derivadaPiston,x0,10^-8);
    x2=piston_x2(x1,L,a);
+   if(sin(phi)<0)
+      x2=x2*-1; 
+   end
    pintar_piston(a,r,phi,x1,x2);
    x0=x1;
+   %0,L,-L/2,L/2
 end
 
 
